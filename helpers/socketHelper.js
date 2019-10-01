a8c0111513375810796528f16b05c3755d7c48b7
lizavetatkachuk <elis.tkachuk@gmail.com>
2019-10-01 11:09:35 +0300

    Optional removing of outdated seats added.

--- helpers/socketHelper.js
+++ helpers/socketHelper.js
@@ -6,23 +6,39 @@ const { User } = require("../models/User");
 const CONFIG = require("../config/index");
 
 const removeOutdated = async data => {
-  const decoded = jwt.verify(data.token, CONFIG.jwt_encryption);
   try {
-    const user = await User.findById(decoded.userId);
-    const now = moment();
-    const seats = await Seats.find({ user, flight: data.flight });
-    const currentSeats = seats.map(async seat => {
-      if (
-        moment(seat.date)
-          .add(10, "minutes")
-          .isAfter(now) === false
-      ) {
-        const outdated = await Seats.findByIdAndDelete(seat._id);
-      }
-    });
-    return currentSeats;
+    if (data) {
+      const decoded = jwt.verify(data.token, CONFIG.jwt_encryption);
+      const user = await User.findById(decoded.userId);
+      const now = moment();
+      const seats = await Seats.find({ user, flight: data.flight });
+      const currentSeats = seats.map(async seat => {
+        if (
+          moment(seat.date)
+            .add(10, "minutes")
+            .isAfter(now) === false
+        ) {
+          const outdated = await Seats.findByIdAndDelete(seat._id);
+        }
+      });
+      return currentSeats;
+    } else {
+      const now = moment();
+      const seats = await Seats.find();
+      const currentSeats = seats.map(async seat => {
+        if (
+          moment(seat.date)
+            .add(10, "minutes")
+            .isAfter(now) === false
+        ) {
+          const outdated = await Seats.findByIdAndDelete(seat._id);
+        }
+      });
+      return currentSeats;
+    }
   } catch (error) {
     return null;
   }
 };
+
 module.exports.removeOutdated = removeOutdated;
